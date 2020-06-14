ef28b49b47073fa8cec8e9dd4a105fe35ceb1632
jinchen-hu <jinchenhu.luke@gmail.com>
2020-06-13 22:04:43 -0400

    up

--- src/task1.py
+++ src/task1.py
@@ -1,26 +1,36 @@
 import pandas as pds
 import nltk
+import string
 nltk.download('punkt')
 # Definie the path of data set
 DATA_FILE_PATH = '../dataset/hns_2018_2019.csv'
 # Read the data as DataFrame type, use 'Created At' as row labels/indices,
 # extract columns 'Title', 'Post Type', and 'Created At',
-# set column 'Created At' as date times
 data = pds.read_csv(DATA_FILE_PATH,
                     index_col='Created At',
                     usecols=['Title', 'Post Type', 'Created At'])
 
 # Extract data in 2018 as Trainning data
 training_data = data.loc[data.index < '2019']
-test_data = data.loc[data.index >= '2019']
+testing_data = data.loc[data.index >= '2019']
 print(training_data.shape)
-print(test_data.shape)
+print(testing_data.shape)
+# A set storing removed words
+removed_words = set()
+# A set storing vocabularies
+voc = set()
+# A map<key=type, value = count>
+type_count = {}
 
-temp = data.iloc[2]['Title']
-print(temp)
+for index, row in training_data.iterrows():
+    type_name = row['Post Type']
+    if type_name in type_count.keys():
+        type_count[type_name] += 1
+    else:
+        type_count[type_name] = 1
 
-print(nltk.word_tokenize(temp))
 
+print(type_count)
 
 
 
